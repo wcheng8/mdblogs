82bd387b2bd10790dddfde3944744706d0431ebb
HarrisonPowell <harrisonpowell365@gmail.com>
2020-07-23 14:28:37 +1000

    setup mongodb atlas and working

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,14 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 require("dotenv").config();
+const mongoose = require("mongoose");
 
 //app
 const app = express();
+
+//database
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true })
+  .then(() => console.log("Database connected"));
 
 //middlewares
 app.use(morgan("dev"));
