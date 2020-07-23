6e52a7d3ff1452704603501ed7aed69e1bc6a35b
HarrisonPowell <harrisonpowell365@gmail.com>
2020-07-23 14:51:08 +1000

    routes file setup and working

--- backend/server.js
+++ backend/server.js
@@ -6,26 +6,32 @@ const cors = require("cors");
 require("dotenv").config();
 const mongoose = require("mongoose");
 
+// bringing in the routes
+const blogRoutes = require("./routes/blog");
+
 //app
 const app = express();
 
 //database
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true })
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("Database connected"));
 
 //middlewares
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(blogRoutes);
+
 //cors
 if ((process.env.NODE_ENV = "development")) {
   app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
 }
-
-//routes
-app.get("/api", (req, res) => {
-  res.json({ time: Date().toString() });
-});
 
 //port
 const port = process.env.PORT || 8000;
