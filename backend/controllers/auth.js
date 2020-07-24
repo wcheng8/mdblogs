13ecbeda144c94f51c27e29a415ed3d55c5cbab3
HarrisonPowell <harrisonpowell365@gmail.com>
2020-07-24 15:02:50 +1000

    starting to set up some front end

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,6 +1,71 @@
+const User = require("../models/user");
+const shortId = require("shortid");
+const jwt = require("jsonwebtoken");
+const expressJwt = require("express-jwt");
+
 exports.signup = (req, res) => {
-  const { name, email, password } = req.body;
-  res.json({
-    user: { name, email, password },
+  User.findOne({ email: req.body.email }).exec((err, user) => {
+    if (user) {
+      return res.status(400).json({
+        error: "Email is taken",
+      });
+    }
+    const { name, email, password } = req.body;
+    let username = shortId.generate();
+    let profile = `${process.env.CLIENT_URL}/profile/${username}`;
+
+    let newUser = new User({ name, email, password, profile, username });
+    newUser.save((err, success) => {
+      if (err) {
+        return res.status(400).json({
+          error: err,
+        });
+      }
+      res.json({
+        message: "Signup Success, please Signin.",
+      });
+    });
   });
 };
+
+exports.signin = (req, res) => {
+  const { email, password } = req.body;
+  //checking if user exists
+  User.findOne({ email }).exec((err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "That email does not exist"
+      });
+    }
+    //auth user
+    if (!user.authenticate(password)) {
+      return res.status(400).json({
+        error: "That email and Password do not match"
+      });
+    }
+    // g a JWT and send
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.cookie('token', token, { expiresIn: "1d" });
+    const { _id, username, name, email, role } = user;
+    return res.json({
+      token,
+      user: { _id, username, name, email, role }
+    });
+  });
+};
+
+exports.signout = (req, res) => {
+  res.clearCookie("token");
+  res.json({
+    message: "Signout successful"
+  });
+};
+
+exports.requireSignin = expressJwt({
+  secret: process.env.JWT_SECRET,
+  algorithms: ["HS256"],
+  userProperty: "auth",
+});
--- backend/models/user.js
+++ backend/models/user.js
@@ -33,7 +33,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    salt: Number,
+    salt: String,
     about: {
       type: String,
     },
@@ -49,4 +49,39 @@ const userSchema = new mongoose.Schema(
   { timestamp: true }
 );
 
-module.exports = mongoose.model('User', userSchema);
+userSchema
+  .virtual("password")
+  .set(function (password) {
+    this._password = password;
+
+    this.salt = this.makeSalt();
+
+    this.hashed_password = this.encryptPassword(password);
+  })
+
+  .get(function () {
+    return this._password;
+  });
+
+userSchema.methods = {
+  authenticate: function (plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password;
+  },
+
+  encryptPassword: function (password) {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha1", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
+  },
+  makeSalt: function () {
+    return Math.round(new Date().valueOf() * Math.random()) + "";
+  },
+};
+
+module.exports = mongoose.model("User", userSchema);
