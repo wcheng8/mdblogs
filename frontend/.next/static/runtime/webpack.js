1ff13877ec8c296e770a3e9e846d30cacf173be2
Wilkins Cheng <wilkinscheng@MacBook-Pro.hub>
2020-07-30 12:09:18 +1000

    added a dropdown for MD helper

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "78dd550ff7bd4a37d009";
+/******/ 	var hotCurrentHash = "907f44c2c8c03859c03e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
